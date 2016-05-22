a13874a041bdbdd92a7a226a0306e7d1efea441e
saulocamposi <saulocamposi@gmail.com>
2016-05-22 20:34:57 -0300

    Updated test task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 var gulp = require('gulp');
+var Server = require('karma').Server;
 
-gulp.task('default',function(){
-  console.log("hello gulp");
+gulp.task('test',function(done){
 
+  new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true,
+  }, done).start();
+
+  console.log("Test Karma-Gulp");
+});
+
+
+gulp.task('default',function(done){
+  console.log("Gulp default using test");
 });
