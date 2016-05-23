bbe04ef6b6735a28df0b3479025aa4b75e1ff2f4
saulocamposi <saulocamposi@gmail.com>
2016-05-23 00:43:48 -0300

    Updated files to be included

--- karma.conf.js
+++ karma.conf.js
@@ -2,11 +2,20 @@ module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine'],
-    //files:['src/app/angular-components/google-chart/']
     files:[
-      'src/**/*.js',
-      'src/**/test_*.js',
-  ]
+      //'src/**/*.js',
+      //'src/**/test_*.js',
+      'node_modules/angular/angular.min.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'src/app/angular-components/google-chart/module.js',
+      'src/app/angular-components/google-chart/bussiness.js',
+      'src/app/angular-components/google-chart/linechart.js',
+      'src/app/angular-components/google-chart/chart.js',
+      'src/app/angular-components/google-chart/test_*.js',
+    ],
+    reporters: ['spec'],
+    browsers: ['PhantomJS']
+
 
 
   });
