8c757a293a1514de11a36aaf35d57f2209fdc44b
saulocamposi <saulocamposi@gmail.com>
2016-05-23 17:38:33 -0300

    Updated inserted others log level

--- karma.conf.js
+++ karma.conf.js
@@ -5,16 +5,27 @@ module.exports = function(config) {
     files:[
       //'src/**/*.js',
       //'src/**/test_*.js',
-      'node_modules/angular/angular.min.js',
+      //'node_modules/**/',
+      'node_modules/angular/angular.js',
+      //'node_modules/angular/angular-google-chart/ng-google-chart.js',
       'node_modules/angular-mocks/angular-mocks.js',
-      'src/app/angular-components/google-chart/module.js',
-      'src/app/angular-components/google-chart/bussiness.js',
-      'src/app/angular-components/google-chart/linechart.js',
-      'src/app/angular-components/google-chart/chart.js',
-      'src/app/angular-components/google-chart/test_*.js',
+       //'node_modules/angular-route/angular-route.min.js',
+      //'src/app/angular-components/google-chart/module.js',
+      //'src/app/angular-components/google-chart/bussiness.js',
+      //'src/app/angular-components/google-chart/linechart.js',
+      //'src/app/angular-components/google-chart/chart.js',
+      //'src/app/angular-components/google-chart/test_*.js',
+      //'src/app/angular-basic-version-1/firstController.js',
+      //'src/app/angular-basic-version-1/firstController.js',
+      //'src/app/angular-basic-version-1/test_*.js',
+      'src/app/angular-restangular/*.js'
+
     ],
     reporters: ['spec'],
-    browsers: ['PhantomJS']
+    browsers: ['PhantomJS'],
+    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_ERROR,
+    logLevel: config.WARN
 
 
 
