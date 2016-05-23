245aeb6ad6106e9d14a4fc6a3a15a274fa5e161e
saulocamposi <saulocamposi@gmail.com>
2016-05-23 00:44:20 -0300

    Fixed changed name controller

--- src/app/angular-components/google-chart/linechart.js
+++ src/app/angular-components/google-chart/linechart.js
@@ -1,7 +1,7 @@
-var firstController = function($scope, bussiness,chartservice ){
+var linechartCtrl = function($scope, bussiness,chartservice ){
   $scope.hello = 'Welcome to play with googlechart';
   $scope.helloWorld = bussiness;
   $scope.chart = chartservice;
 };
 
-app.controller('firstController',['$scope','bussiness','chartservice',firstController] );
+app.controller('linechartCtrl',['$scope','bussiness','chartservice', linechartCtrl] );
