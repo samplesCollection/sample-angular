0e8c03ee06ed68238575cfaeaa25f041bc9893c1
saulocamposi <saulocamposi@gmail.com>
2016-05-22 21:28:58 -0300

    Updated files required

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,13 @@
 module.exports = function(config) {
   config.set({
-    basePath: '../..',
+    basePath: '',
     frameworks: ['jasmine'],
-    //...
+    //files:['src/app/angular-components/google-chart/']
+    files:[
+      'src/**/*.js',
+      'src/**/test_*.js',
+  ]
+
+
   });
 };
