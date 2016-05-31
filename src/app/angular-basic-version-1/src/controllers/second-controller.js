40657f96c23efa7eaf4a1044245b045a26751ef2
saulocamposi <saulocamposi@gmail.com>
2016-05-30 21:54:07 -0300

    Added secondcontroller

--- src/app/angular-basic-version-1/src/controllers/second-controller.js
+++ src/app/angular-basic-version-1/src/controllers/second-controller.js
@@ -1,3 +1,10 @@
-angular.module('secondmodule',[])
-.controller('secondcontroller', ['myService', function($scope){} ])
-.factory('myService', function(){return "myService"});
+angular.module('secondmodule', [])
+  .controller('secondcontroller', ['$scope', 'myService', function($scope, myService) {
+    $scope.mycontroller = "mycontroller";
+    console.log(myService);
+    console.log("service");
+    $scope.myservice = myService;
+  }])
+  .factory('myService', ['$http', function($http) {
+    return $http.jsonp("https://api.blinktrade.com/api/v1/BRL/ticker?crypto_currency=BTC&callback=JSON_CALLBACK").then(function(response) {});
+  }]);
