911825a818a69b2f48bc086bd24247977b271887
saulocamposi <saulocamposi@gmail.com>
2016-06-01 00:11:01 -0300

    Updated promise response

--- src/app/angular-basic-version-1/src/controllers/second-controller.js
+++ src/app/angular-basic-version-1/src/controllers/second-controller.js
@@ -3,12 +3,16 @@ angular.module('secondmodule', [])
     $scope.mycontroller = "mycontroller";
     console.log(myService);
     console.log("service");
-    $scope.myservice = myService;
+    var data = myService.data;
+    $scope.myservice = myService.data;
+    console.log("Here is my data "+data);
+
   }])
   .factory('myService', ['$http', function($http) {
     return new Promise(function(error,sucess){
-      $http.jsonp("https://api.blinktrade.com/api/v1/BRL/ticker?crypto_currency=BTC&callback=JSON_CALLBACK").then(function(response) {
-        
+      return $http.jsonp("https://api.blinktrade.com/api/v1/BRL/ticker?crypto_currency=BTC&callback=JSON_CALLBACK").then(function(response) {
+        console.log("Inside Response " + response.data.toString());
+        return response.data;
       });
     });
   }]);
