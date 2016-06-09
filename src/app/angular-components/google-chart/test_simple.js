9afc81471cabb53e63eaae6b6cad8c9d3e4443d1
saulocamposi <saulocamposi@gmail.com>
2016-06-08 21:35:45 -0300

    Fix change path

--- src/app/angular-components/google-chart/test_simple.js
+++
@@ -1,8 +0,0 @@
-
-
-describe('hello test', function(){
-  it("should return hello world",function(){
-    console.log("hello world test");
-    expect("helloWorld").toEqual("helloWorld")
-  });
-});
