a96462d7825ba03ca3f67338f5e10847f7c7b8b6
saulocamposi <saulocamposi@gmail.com>
2016-06-13 13:52:35 -0300

    Fix change path

--- src/app/angular-google-chart/linechart.js
+++
@@ -1,7 +0,0 @@
-var linechartCtrl = function($scope, bussiness,chartservice ){
-  $scope.hello = 'Welcome to play with googlechart';
-  $scope.helloWorld = bussiness;
-  $scope.chart = chartservice;
-};
-
-app.controller('linechartCtrl',['$scope','bussiness','chartservice', linechartCtrl] );
