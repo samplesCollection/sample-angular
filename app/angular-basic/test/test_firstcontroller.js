843348eb7944daa6ff1e3d8f0dbd7265cac4d06d
saulocamposi <saulocamposi@gmail.com>
2016-06-14 22:23:04 -0300

    Add test firstcontroller

---
+++ app/angular-basic/test/test_firstcontroller.js
@@ -0,0 +1,10 @@
+describe('firstController', function() {
+
+  var scope, firstController;
+
+  beforeEach(angular.mock.module('app'));
+
+  beforeEach(inject(function(_firsController_) {
+    $firsController = _firsController_;
+  }));
+});
